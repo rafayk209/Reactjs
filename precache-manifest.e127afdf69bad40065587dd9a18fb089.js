b12dc8bc600ea644be39686c1d4c33f8b0fe9d37
rafayk209 <rafayk209@gmail.com>
2021-12-19 17:10:22 +0500

    Updates

---
+++ precache-manifest.e127afdf69bad40065587dd9a18fb089.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca8c61d89431cbd027cbb6b2bf5dc5d",
+    "url": "/reactjs-web/index.html"
+  },
+  {
+    "revision": "689ec0e5844055fa16ed",
+    "url": "/reactjs-web/static/css/2.709e72d0.chunk.css"
+  },
+  {
+    "revision": "247871b99d25beac7da0",
+    "url": "/reactjs-web/static/css/main.9f1082da.chunk.css"
+  },
+  {
+    "revision": "689ec0e5844055fa16ed",
+    "url": "/reactjs-web/static/js/2.9c8f68e6.chunk.js"
+  },
+  {
+    "revision": "859d82b73e973d1fc0746ea1b5ac4cf7",
+    "url": "/reactjs-web/static/js/2.9c8f68e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247871b99d25beac7da0",
+    "url": "/reactjs-web/static/js/main.92fd4f23.chunk.js"
+  },
+  {
+    "revision": "fe4ff58f62d6d2d125a2",
+    "url": "/reactjs-web/static/js/runtime-main.40f87cbf.js"
+  },
+  {
+    "revision": "10c4da590d8f5bade04240cd1060f145",
+    "url": "/reactjs-web/static/media/food.10c4da59.jpg"
+  },
+  {
+    "revision": "9cd073a636edaee57974e82e14a85da0",
+    "url": "/reactjs-web/static/media/image1.9cd073a6.jpg"
+  },
+  {
+    "revision": "c8ebd822dfdc312f5e6da35920fa6740",
+    "url": "/reactjs-web/static/media/logo.c8ebd822.png"
+  }
+]);
